db34d23debd39a1f82c39353c650cda2633b3877
Andrew <reznichenko.a200212@gmail.com>
2025-07-23 19:37:24 +0300

    jv-checked-exception try4

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
 
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!password.equals(repeatPassword)) {
-            if (password.toCharArray().length < 10) {
+        if (!password.equals(repeatPassword) || password.toCharArray().length < 10) {
                 throw new PasswordValidationException("Wrong passwords");
-            }
         }
     }
 }
