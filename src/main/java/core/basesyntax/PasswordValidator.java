efc398ee9892b39bab0689d0bd51f3387add73de
Andrew <reznichenko.a200212@gmail.com>
2025-07-24 18:29:38 +0300

    jx-checked-exception try13

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.length() < 10 || !password.equals(repeatPassword)) {
+        if (password.length() < 10 || !password.equals(repeatPassword) || password.equals(repeatPassword.equals(null))) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
